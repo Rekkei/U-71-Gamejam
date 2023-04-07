d11a2ceaa0887d647aafeedb8fda876a0be768c1
ozlem1dev <ozlem.dev@hotmail.com>
2023-04-08 02:50:12 +0300

    Revert "Merge branch 'test' into ozlem1"
    This reverts commit c382ad72f97fb2b226214913a76f7ae27189488e, reversing
    changes made to ad7be2f757c87b01a2964b65419d45ebd567b5ba.

--- Assets/scripts/pointsystem.cs
+++ Assets/scripts/pointsystem.cs
@@ -4,35 +4,15 @@ using UnityEngine;
 
 public class pointsystem : MonoBehaviour
 {
-
-    Camera cam;
-    public LayerMask mask;
-
+    // Start is called before the first frame update
     void Start()
     {
-        cam = Camera.main;
+        
     }
 
+    // Update is called once per frame
     void Update()
     {
         
-        Vector3 mousePos = Input.mousePosition;
-        mousePos.z = 100f;
-        mousePos = cam.ScreenToWorldPoint(mousePos);
-        Debug.DrawRay(transform.position, mousePos - transform.position,
-        Color.blue);
-
-        if (Input.GetMouseButtonDown(0))
-        {
-            Ray ray = cam.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit, 100, mask))
-            {
-                Debug.Log(hit.transform.name);
-                hit.transform.GetComponent<Renderer>().material.color =
-                Color.red;
-            }
-        }
     }
 }
