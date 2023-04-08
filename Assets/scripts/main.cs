22d20b731155799d200afe8277ac7757ad273e01
Tuna Yavuz <tunayavuz.95@hotmail.com>
2023-04-08 16:20:31 +0300

    statlar eklendi

--- Assets/scripts/main.cs
+++ Assets/scripts/main.cs
@@ -2,19 +2,32 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.UI;
 
 
+
 public class main : MonoBehaviour
 {
-    public GameObject popup, popup2, popup3;
-    public TMP_Text TEXT1, TEXT2, TEXT3, TEXT4;
+    public GameObject popup, popup2, popup3,popup4;
+    public TMP_Text TEXT1, TEXT2, TEXT3, TEXT4, TEXT5;
     public Sprite[] kalanzaman;
-    public  Image bar;
+    public Image bar;
+    public Sprite[] akademibar;
+    public Image akademimage;
+    public Sprite[] dersbar;
+    public Image dersimage;
+    public Sprite[] sosyalbar;
+    public Image sosyalimage;
     public static int kalanzamanbar = 0;
+    public static int dersstat = 0;
+    public static int akademistat = 0;
+    public static int sosyalstat = 0;
     public Sprite[] moonandsunsprite;
     public Image moonandsun123;
+    public bool mustsleep = false;
+    
 
 
 
@@ -27,7 +40,7 @@ public class main : MonoBehaviour
 
     public void button1durum()
     {
-        
+
         if (clickpointer.durum == 1)
         {
             akademi();
@@ -61,20 +74,19 @@ public class main : MonoBehaviour
     }
 
 
-    
-
-
-
-
     public void okul()
     {
+
         popup.SetActive(false);
         TEXT4.text = "okula gidiyorsun";
         TEXT3.text = "okula gidip akademik basarini arttirdin";
+        Debug.Log(dersstat);
         popup2.SetActive(true);
     }
     public void disari()
     {
+        sosyalstat += 2;
+        kalanzamanbar -= 2;
         popup.SetActive(false);
         TEXT4.text = "disari cikiyorsun";
         TEXT3.text = "disari cikip arkadaslarin ile eglendin populerlik artti";
@@ -82,6 +94,7 @@ public class main : MonoBehaviour
     }
     public void uyu()
     {
+        kalanzamanbar = 8;
         popup.SetActive(false);
         TEXT4.text = "yataga gittin";
         TEXT3.text = "guzel bir uyku cekip enerjini yeniledin";
@@ -89,6 +102,8 @@ public class main : MonoBehaviour
     }
     public void akademi()
     {
+        akademistat += 1;
+        kalanzamanbar -= 2;
         popup.SetActive(false);
         TEXT4.text = "oyun ve uygulama akademisi sitesine giris yaptin";
         TEXT3.text = "oyun ve uygulama akademisinde ilerleme kaydettin";
@@ -96,8 +111,10 @@ public class main : MonoBehaviour
     }
     public void ders()
     {
+        dersstat += 2;
+        kalanzamanbar -= 2;
         popup.SetActive(false);
-        TEXT4.text = "ders çalismaya basladin";
+        TEXT4.text = "ders calismaya basladin";
         TEXT3.text = "ders calisip akademik basarini arttirdin";
         popup2.SetActive(true);
     }
@@ -118,8 +135,21 @@ public class main : MonoBehaviour
         StartCoroutine(blackscreen());
         IEnumerator blackscreen()
         {
-            
+
             yield return new WaitForSecondsRealtime(3f);
+            akademimage.sprite = akademibar[akademistat];
+            dersimage.sprite = dersbar[dersstat];
+            sosyalimage.sprite = sosyalbar[sosyalstat];
+            bar.sprite = kalanzaman[kalanzamanbar];
+            if (kalanzamanbar < 5)
+            {
+                moonandsun123.sprite = moonandsunsprite[1];
+
+            }
+            else
+            {
+                moonandsun123.sprite = moonandsunsprite[0];
+            }
             popup3.SetActive(false);
 
 
@@ -129,16 +159,15 @@ public class main : MonoBehaviour
     }
     private void FixedUpdate()
     {
-
-        bar.sprite = kalanzaman[kalanzamanbar];
-        if (kalanzamanbar < 5)
+        if (kalanzamanbar <= 0)
         {
-            moonandsun123.sprite = moonandsunsprite[1];
-
+            mustsleep = true;
         }
-        else
+        if(mustsleep)
         {
-            moonandsun123.sprite = moonandsunsprite[0];
+            popup4.SetActive(false);
+            TEXT5.text = "enerjin kalmadý uyumak zorundasýn";
+            popup4.SetActive(true);
         }
     }
 }
